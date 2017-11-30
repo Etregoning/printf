f6176cb9defcc51c8bbaf0829174c3539e525182
Elliot Tregoning <etregoni@e1z2r5p17.42.us.org>
2017-11-29 20:46:16 -0800

    doodoo butt

--- src/formatting.c
+++ src/formatting.c
@@ -1,24 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   formatting.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: etregoni <etregoni@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/29 16:38:17 by etregoni          #+#    #+#             */
-/*   Updated: 2017/11/29 16:38:21 by etregoni         ###   ########.fr       */
+/*   Created: 2017/11/29 20:32:49 by etregoni          #+#    #+#             */
+/*   Updated: 2017/11/29 20:32:50 by etregoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
 
-int	ft_printf(const char restrict *format, ...)
-{
-	va_list	arg;
-
-	va_start(arg, format);
-	while (*format)
-	{
-		format_check(arg);
-	}
-}
